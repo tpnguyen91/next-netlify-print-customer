ea5f3d9585c22964026555ec8364faf2491c69ad
NguyenPhuongfreeC <tpnguyen91@gmail.com>
2023-09-19 00:30:14 +0700

    config project

---
+++ components/PrintComponent.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+const PrintComponent = React.forwardRef((props, ref) => {
+  return (
+    <div className="w-[576px] h-[384px] bg-white border" ref={ref}>
+      My cool content here!
+    </div>
+  )
+})
+
+PrintComponent.displayName = 'PrintComponent'
+
+export default PrintComponent
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
-import '@styles/globals.css'
+import React from 'react'
+import '../styles/globals.css'
 
 function Application({ Component, pageProps }) {
   return <Component {...pageProps} />
