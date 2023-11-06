a88d867120b6ffc733efe53a403af82ce83e19e9
NguyenPhuongfreeC <tpnguyen91@gmail.com>
2023-11-06 22:08:35 +0700

    fix bugs

--- src/components/PrintComponentFrom.tsx
+++ src/components/PrintComponentFrom.tsx
@@ -9,19 +9,19 @@ const PrintComponentFrom = React.forwardRef((props, ref) => {
         className="w-[576px] max-h-[384px] h-[384px] bg-white overflow-hidden px-4 py-4"
         //@ts-ignore
         ref={ref}>
-        <div className="grid grid-cols-[110px_1fr] w-full border-dashed	border-2 px-1 py-1 rounded">
+        <div className="grid grid-cols-[110px_1fr] w-full border-dashed border-blue-gray-700	border-2 px-1 py-1 rounded">
           <div className="w-[100px] underline">Người gửi:</div>
           <div className="flex flex-row items-center">
             <p className="font-bold text-[40px]">C Thuỷ</p>
             <p className="font-medium text-[20px] ml-4"> 0909721166</p>
           </div>
         </div>
-        <div className="grid grid-cols-[110px_1fr] w-full border-dashed	border-2 px-1 py-1 rounded mt-2">
+        <div className="grid grid-cols-[110px_1fr] w-full border-dashed border-blue-gray-700	border-2 px-1 py-1 rounded mt-2">
           <div className="w-[100px] underline">Người nhận:</div>
           <div className="">
             <div>
               <p className="font-bold text-[50px]">{name}</p>
-              <p className="font-medium text-[20px]">{phone}</p>
+              <p className="font-medium text-5xl">{phone}</p>
             </div>
             <div className="font-medium text-[30px]">{address}</div>
           </div>
